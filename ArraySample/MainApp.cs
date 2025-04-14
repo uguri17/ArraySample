91afd61e5175dd6c3c0faa8afb73e5b4a87697ee
RND-HW3 <RND-HW3@RND-HW3>
2025-04-14 16:35:35 +0900

    초기 커밋.

---
+++ ArraySample/MainApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace ArraySample
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[5];
+            scores[0] = 10;
+            scores[1] = 20;
+            scores[2] = 30;
+            scores[3] = 40;
+            scores[4] = 50;
+            
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"scores[{i}]: {scores[i]}");
+            }
+              
+                          
+            int sum = 0;            
+            foreach (int score in scores)
+            {
+                sum += score;
+            }
+            Console.WriteLine($"Total: {sum}");
+            int average = sum / scores.Length;
+
+            Console.WriteLine($"Average Score : {average}");
+        }
+    }
+}
